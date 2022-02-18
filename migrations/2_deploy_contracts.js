cc7c9151b3877da1c1fcb73e5f173730513ccbd1
Moonshot <>
2022-02-18 16:47:54 +0100

    refactor

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 require( 'dotenv' ).config();
 
 const token = artifacts.require("Moonshot");
-const drop = artifacts.require("DropMoonshot");
-const trader = artifacts.require("MoonshotTrader");
+const claim = artifacts.require("DropMoonshot");
+const buy = artifacts.require("MoonshotTrader");
 
 const migration = async function( deployer, network, accounts ) {
         await Promise.all([
@@ -13,7 +13,7 @@ const migration = async function( deployer, network, accounts ) {
 module.exports = migration;
 
 async function deployContracts(deployer,network,accounts) {
-     const moonshot = await deployer.deploy(token);
-     const airdrop = await deployer.deploy(drop);
-     const buy = await deployer.deploy( trader );
+     const moonshotContract = await deployer.deploy(token);
+     const claimContract = await deployer.deploy(claim);
+     const buyContract = await deployer.deploy(buy);
 }
