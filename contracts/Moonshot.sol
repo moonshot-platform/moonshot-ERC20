102c4665cc72a1d345fabaf990ce2ce2e2101c27
Moonshot <>
2022-02-21 13:13:00 +0100

    target bsc mainnet router

--- contracts/Moonshot.sol
+++ contracts/Moonshot.sol
@@ -759,10 +759,10 @@ contract Moonshot is Context, IERC20, Ownable {
         _rOwned[_msgSender()] = _rTotal;
         
         // BSC MainNet, Pancakeswap Router
-        //IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
 
         // Ropsten, Uniswap Router
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+        //IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
 
         // BSC TestNet
         //IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1);
